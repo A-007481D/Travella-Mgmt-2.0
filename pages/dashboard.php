3693af612104750cda5a9a006410a625ed84d40e
Mahdi732 <ey449751@gmail.com>
2024-12-26 13:14:02 +0100

    create page registre and php code

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,1 +1,15 @@
-<?php require_once("../processes/userProcess.php")?>
+<?php 
+// require_once("../processes/userProcess.php");
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>oefzeifnzeovezfezzkvbsojvsvnfdvonvnodfvidnvodvjnkdb,sfdibnfdobfd</h1>
+</body>
+</html>
